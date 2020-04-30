4baf0011eae361a5359cacc00948782e3dc766c4
PADMALAYA RAWAL <60266976+padmalaya-rawal@users.noreply.github.com>
2020-04-30 20:31:45 +0530

    General method to add 2 no
    This is the general method to add two numbers

---
+++ Add two numbers.py
@@ -0,0 +1,5 @@
+number1 = int(input('Number 1: '))   #Enter number1
+number2 = int(input('Number 2: '))   #Enter number2
+sum =number1 + number2               #logic of the program
+
+print(f"Sum of {number1} and {number2} is {sum}") #print the result using formatted string method
